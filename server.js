e7bd273cb99e1fcb7ffd9c92f3b7597f001dd128
cfcrotty <carafeliselana@gmail.com>
2019-04-17 09:43:59 -0700

    update

--- server.js
+++ server.js
@@ -8,18 +8,20 @@ const cheerio = require("cheerio");
 const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const databaseUrl = "scraper";
-const collections = ["scrapedData"];
 
+// Require all models
+const db = require("./models");
 
-const db = mongojs(databaseUrl, collections);
-db.on("error", error => {
-    console.log("Database Error:", error);
-});
+// const databaseUrl = "scraper";
+// const collections = ["scrapedData"];
+// const db = mongojs(databaseUrl, collections);
+// db.on("error", error => {
+//     console.log("Database Error:", error);
+// });
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
 mongoose.connect(MONGODB_URI);
+var mc = mongoose.connection;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -32,13 +34,28 @@ app.engine(
     })
 );
 app.set("view engine", "handlebars");
-// Main route (simple Hello World Message)
+
 app.get("/", (req, res) => {
-    res.status(200).render("index");
+    db.Article.find({}, (error, found) => {
+        if (error) {
+            console.log(error);
+            res.status(500).json(error);
+        } else {
+            res.status(200).render("index",{data: found});
+        }
+    });
 });
 
-app.get("/all", (req, res) => {
-    db.scrapedData.find({}, (error, found) => {
+app.get("/api/clear", (req, res) => {
+    mc.dropCollection('articles',(err,data)=>{
+        res.status(200).json("clear");
+    });
+});
+
+app.get("/api/headlines", (req, res) => {
+    let flag = false;
+    if (req.body.saved) flag=true;
+    db.Article.find({}, (error, found) => {
         if (error) {
             console.log(error);
         } else {
@@ -47,16 +64,13 @@ app.get("/all", (req, res) => {
     });
 });
 
-app.get("/save", (req, res) => {
+app.get("/api/fetch", (req, res) => {
     // axios.get("https://www.zaful.com/dresses-e_5/?innerid=6002&policy_key=B").then(response => {
-
     //     let $ = cheerio.load(response.data);
     //     let results = [];
-
     //     $(".img-hover-wrap .js_list_link").each(function (i, element) {
     //         let title = $(element).attr("title");
     //         let link = $(element).children().attr("data-original");
-
     //         results.push({
     //             link: link,
     //             title: title
@@ -64,32 +78,38 @@ app.get("/save", (req, res) => {
     //     });
     //     db.scrapedData.insert(results);
     //     res.json(results);
-
     // });
 
     axios.get("https://www.nytimes.com").then(response => {
         let $ = cheerio.load(response.data);
-
-        // An empty array to save the data that we'll scrape
         let results = [];
 
-        // Select each element in the HTML body from which you want information.
-        // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-        // but be sure to visit the package's npm page to see how it works
-        $("article").each(function (i, element) {
+        $(".css-6p6lnl").each(function (i, element) { //css-8atqhb
+            //let title = $(element).children().text();
+            //let title = $(element).find("span").text();
+            //let link = $(element).find("a").attr("href");
 
-            let title = $(element).children().text();
+            let title = $(element).find("h2").text();
+            let summary = $(element).find("ul").find("li").text();
             let link = $(element).find("a").attr("href");
-
-            // Save these results in an object that we'll push into the results array we defined earlier
-            results.push({
-                title: title,
-                link: link
-            });
+            if (summary) {
+                results.push({
+                    title: title,
+                    link: link,
+                    summary: summary
+                });
+            }
         });
 
-        db.scrapedData.insert(results);
-        res.json(results);
+        db.Article.create(results)
+            .then(results => {
+                res.status(200).json(results);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+        // console.log(results);
+        // res.send("test");
     });
 })
 
